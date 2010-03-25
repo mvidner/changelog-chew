730b1d534808d363ea8ca83abd356d210ce72ee5
Martin Vidner <martin@vidner.net>
2010-03-25 18:11:53 +0100

    Made it work with `rpm -qa`. Licensed as MIT.

--- changelog-aggregator.rb
+++ changelog-aggregator.rb
@@ -1,6 +1,9 @@
 #! /usr/bin/env ruby
+# aggregate changelogs for RPMs: either -q --changelog or (TODO) SUSE .changes
+# license: http://en.wikipedia.org/wiki/MIT_License
+
 require "date"
-require "pp"
+# require "pp"
 
 class Change
   # what is being changed (usu. String)
@@ -14,24 +17,36 @@ class Change
   # how it changed, the message, single string (including trailing \n)
   attr_accessor :description
 
+  def to_s
+    "----\n" + "#{object} @#{timestamp} #{author}\n" + description.to_s
+  end
 end
 
 # parses an IO object
 # returns a list of Changes (where :object is nil)
 # TODO optionally yield them instead
-def parse_rpm_changelog(io)
+def parse_rpm_changelog(io, object = nil)
   items = []
   item = nil
   io.each_line do |line|
     next if line =~ /^\S*$/
-    if line =~ /^\* (.*) (\S*)/
+    # author can contain space :-/
+    # heuristic: date ends in a 4-digit year
+    if line =~ /^\* (.*\d\d\d\d) (.*)/
       # finish previous item
       items << item unless item.nil?
       # new item
       item = Change.new
-      item.timestamp = Date::parse $1
+      begin
+        item.timestamp = Date::parse $1
+      rescue ArgumentError
+        puts " HUH #{object}: #{line}"
+        item.timestamp = Date.today
+        # TODO recover better
+      end
       item.author = $2
       item.lineno = io.lineno
+      item.object = object
     elsif not item.nil?
       # add to description of current item
       item.description ||= ""
@@ -43,11 +58,25 @@ def parse_rpm_changelog(io)
   items
 end
 
-
-# usage: LANG=C rpm -q --changelog foo | ruby changelog-aggregator.rb
-changes = parse_rpm_changelog $stdin
-changes.each do |i|
-  puts "----"
-  puts "#{i.author}: #{i.timestamp} L#{i.lineno}"
-  printf "%s", i.description    # no trailing \n
+def main
+  all = []
+  rpmnames = `rpm -qa`.split    # TODO replace -a by ARGV
+  puts "#{rpmnames.size} packages"
+  ENV["LANG"] = "C"             # parse C dates
+  # TODO paralellize the popens?
+  rpmnames.each do |rpm|
+    io = IO.popen("rpm -q --changelog #{rpm}")
+    all += parse_rpm_changelog(io, rpm)
+    io.close
+    print "."
+    $stdout.flush
+  end
+  print "\n"
+  puts "#{all.size} changes"
+  sorted = all.sort_by {|e| [e.timestamp, e.lineno] }
+  sorted.reverse_each {|e| print e }
 end
+
+main
+
+# TODO take examples of (weird) changelogs, make tests
